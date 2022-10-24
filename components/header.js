1512b8fd5c87a874d403f686b721c493ae477381
S David Kong <SDAVIDKONG@gmail.com>
2022-10-24 17:46:12 -0400

    added layout and styling

---
+++ components/header.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "../styles/Home.module.css";
+
+const Header = () => {
+  return <h1 className={styles.h1}>Olympus Etheria</h1>;
+};
+
+export default Header;
