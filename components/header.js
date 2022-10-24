2226a20fd5831a1690a441cd2bfdd21f502d227b
S David Kong <SDAVIDKONG@gmail.com>
2022-10-24 18:36:12 -0400

    added header gradient

--- components/header.js
+++ components/header.js
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "../styles/Home.module.css";
 
 const Header = () => {
-  return <h1 className={styles.h1}>Olympus Etheria</h1>;
+  return <h1 className={styles.h1}>~Olympus Etheria~</h1>;
 };
 
 export default Header;
