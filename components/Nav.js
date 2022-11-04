b376f7aa9b13ce7de9c34a7768b6bcfca5d1100e
S David Kong <SDAVIDKONG@gmail.com>
2022-11-03 20:41:08 -0400

    active classname on nav

--- components/Nav.js
+++ components/Nav.js
@@ -1,19 +1,45 @@
 import React from "react";
 import Link from "next/link";
 import styles from "../styles/Home.module.css";
+import { useRouter } from "next/router";
 
 const Nav = () => {
+  const router = useRouter();
   return (
     <nav className={styles.nav}>
       <ul className={styles.nav}>
-        <li className={styles.navlink}>
-          <Link href="/">Home</Link>
+        <li>
+          <Link href="/">
+            <a
+              className={
+                router.pathname == "/" ? styles.active : styles.navlink
+              }
+            >
+              Home
+            </a>
+          </Link>
         </li>
         <li className={styles.navlink}>
-          <Link href="/gallery">Gallery</Link>
+          <Link href="/gallery">
+            <a
+              className={
+                router.pathname == "/gallery" ? styles.active : styles.navlink
+              }
+            >
+              Gallery
+            </a>
+          </Link>
         </li>
         <li className={styles.navlink}>
-          <Link href="/info">Info</Link>
+          <Link href="/info">
+            <a
+              className={
+                router.pathname == "/info" ? styles.active : styles.navlink
+              }
+            >
+              Info
+            </a>
+          </Link>
         </li>
       </ul>
     </nav>
