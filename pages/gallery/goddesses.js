5564fa76fc9cca97034b82c42ef72857be825798
S David Kong <SDAVIDKONG@gmail.com>
2022-11-05 01:07:57 -0400

    font size

--- pages/gallery/goddesses.js
+++ pages/gallery/goddesses.js
@@ -1,21 +1,97 @@
 import GalleryNav from "../../components/GalleryNav";
 import styles from "../../styles/Home.module.css";
+import GalerryImage from "../../components/GalleryImage";
 
 export default function Home() {
   return (
     <>
       <GalleryNav />
-      <div className={styles.main}>
-        GALLERY COMING SOON
-        {/* <div className={styles.heroBorder}>
-        <img
-          className={styles.hero}
-          src="/god11.png"
-          alt="An image of a badass african god surrounded by ether"
+      <div className={styles.galleryMain}>
+        <GalerryImage img="/goddesses/01.png" name="Athena" />
+        <GalerryImage img="/goddesses/03JunoTheCloudMother.png" name="Juno" />
+        <GalerryImage
+          img="/goddesses/07LuciaTheGeminiSister.png"
+          name="Lucia"
         />
-      </div> */}
-        {/* <h2>Coming Soon</h2>
-      <h2>November 2022</h2> */}
+        <GalerryImage
+          img="/goddesses/08MeredithTheGeminiSister.png"
+          name="Meredith"
+        />
+        <GalerryImage
+          img="/goddesses/10AdamarisTheManifestor.png"
+          name="Adamaris"
+        />
+        <GalerryImage img="/goddesses/13JomalaTheElegant.png" name="Jomala" />
+        <GalerryImage img="/goddesses/15EchoTheScientist.png" name="Echo" />
+        <GalerryImage
+          img="/goddesses/17DaphneTheUnobtainable.png"
+          name="Daphne"
+        />
+        <GalerryImage img="/goddesses/18AnsaTheWindMother.png" name="Ansa" />
+        <GalerryImage
+          img="/goddesses/22RafiyaTheStoryteller.png"
+          name="Rafiya"
+        />
+        <GalerryImage
+          img="/goddesses/26ArtemisTheHuntLeader.png"
+          name="Artemis"
+        />
+        <GalerryImage img="/goddesses/27GenmeiTheInfinite.png" name="Genmei" />
+        <GalerryImage
+          img="/goddesses/28NefertitiTheCosmic.png"
+          name="Nefertiti"
+        />
+        <GalerryImage
+          img="/goddesses/30LakshmiTheGenerous.png"
+          name="Lakshmi"
+        />
+        <GalerryImage
+          img="/goddesses/32UrsulaTheMirageMaker.png"
+          name="Ursula"
+        />
+        <GalerryImage img="/goddesses/37LisaTheGoldenSiren.png" name="Lisa" />
+        <GalerryImage
+          img="/goddesses/38MinervaTheImperatorClone.png"
+          name="Minerva"
+        />
+        <GalerryImage
+          img="/goddesses/41OpheliaTheSkyMother.png"
+          name="Ophelia"
+        />
+        <GalerryImage
+          img="/goddesses/42GuanYinTheProtector.png"
+          name="GuanYin"
+        />
+        <GalerryImage
+          img="/goddesses/45EmalynTheStarReader.png"
+          name="Emalyn"
+        />
+        <GalerryImage img="/goddesses/46DelphiTheOracle.png" name="Delphi" />
+        <GalerryImage img="/goddesses/51SoyulTheAlchemist.png" name="Soyul" />
+        <GalerryImage
+          img="/goddesses/52AnastasiaTheGlamorousSiren.png"
+          name="Anastasia"
+        />
+        <GalerryImage img="/goddesses/56SaoirseTheNoble.png" name="Saorise" />
+        <GalerryImage img="/goddesses/57NzingaTheWise.png" name="Nzinga" />
+        <GalerryImage
+          img="/goddesses/60VeronicaTheGentle.png"
+          name="Veronica"
+        />
+        <GalerryImage
+          img="/goddesses/61DemeterTheDeathMother.png"
+          name="Demeter"
+        />
+        <GalerryImage
+          img="/goddesses/62PersephoneTheDeathBringer.png"
+          name="Persephone"
+        />
+        <GalerryImage
+          img="/goddesses/67GriseldaTheMagician.png"
+          name="Griselda"
+        />
+        <GalerryImage img="/goddesses/68AnimusThePrismatic.png" name="Animus" />
+        <GalerryImage img="/goddesses/70LetoTheEccentric.png" name="leto" />
       </div>
     </>
   );
--- pages/gallery/gods.js
+++ pages/gallery/gods.js
@@ -1,21 +1,48 @@
 import styles from "../../styles/Home.module.css";
 import GalleryNav from "../../components/GalleryNav";
+import GalerryImage from "../../components/GalleryImage";
 
 export default function Home() {
   return (
     <>
       <GalleryNav />
-      <div className={styles.main}>
-        GODS
-        {/* <div className={styles.heroBorder}>
-        <img
-          className={styles.hero}
-          src="/god11.png"
-          alt="An image of a badass african god surrounded by ether"
+      <div className={styles.galleryMain}>
+        <GalerryImage img="/gods/02AskiaTheDisruptor.png" name="Askia" />
+        <GalerryImage img="/gods/05FabianTheCollector.png" name="Fabian" />
+        <GalerryImage img="/gods/06AnandaTheThoughtSeer.png" name="Ananda" />
+        <GalerryImage img="/gods/09LukasTheInterpretor.png" name="Lukas" />
+        <GalerryImage img="/gods/12LuXiaoTheIronWilled.png" name="LuXiao" />
+        <GalerryImage
+          img="/gods/16LinnaeusTheConstructor.png"
+          name="Linnaeus"
         />
-      </div> */}
-        {/* <h2>Coming Soon</h2>
-      <h2>November 2022</h2> */}
+        <GalerryImage img="/gods/19YasukeTheSteelMinded.png" name="Yasuke" />
+        <GalerryImage img="/gods/20AbhinavTheOriginator.png" name="Abhinav" />
+        <GalerryImage img="/gods/21LangsaTheFluidTemporal.png" name="Langsa" />
+        <GalerryImage img="/gods/24SedolTheInventor.png" name="Sedol" />
+        <GalerryImage img="/gods/25RudolphTheIngenious.png" name="Rudolph" />
+        <GalerryImage
+          img="/gods/29AkhenatenTheDiscerning.png"
+          name="Akhenaten"
+        />
+        <GalerryImage img="/gods/01.png" name="Athena The Imperator" />
+        <GalerryImage img="/gods/01.png" name="Athena The Imperator" />
+        <GalerryImage img="/gods/01.png" name="Athena The Imperator" />
+        <GalerryImage img="/gods/01.png" name="Athena The Imperator" />
+        <GalerryImage img="/gods/01.png" name="Athena The Imperator" />
+        <GalerryImage img="/gods/01.png" name="Athena The Imperator" />
+        <GalerryImage img="/gods/01.png" name="Athena The Imperator" />
+        <GalerryImage img="/gods/01.png" name="Athena The Imperator" />
+        <GalerryImage img="/gods/01.png" name="Athena The Imperator" />
+        <GalerryImage img="/gods/01.png" name="Athena The Imperator" />
+        <GalerryImage img="/gods/01.png" name="Athena The Imperator" />
+        <GalerryImage img="/gods/01.png" name="Athena The Imperator" />
+        <GalerryImage img="/gods/01.png" name="Athena The Imperator" />
+        <GalerryImage img="/gods/01.png" name="Athena The Imperator" />
+        <GalerryImage img="/gods/01.png" name="Athena The Imperator" />
+        <GalerryImage img="/gods/01.png" name="Athena The Imperator" />
+        <GalerryImage img="/gods/01.png" name="Athena The Imperator" />
+        <GalerryImage img="/gods/01.png" name="Athena The Imperator" />
       </div>
     </>
   );
