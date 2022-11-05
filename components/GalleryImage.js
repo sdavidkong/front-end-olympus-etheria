8bff25bea19993657406c4f7b4d2c3ca7cc22cc3
S David Kong <SDAVIDKONG@gmail.com>
2022-11-05 13:52:24 -0400

    changed img to next Image

--- components/GalleryImage.js
+++ components/GalleryImage.js
@@ -1,13 +1,16 @@
 import styles from "../styles/Home.module.css";
+import Image from "next/image";
 
 export default function GalerryImage(props) {
   return (
     <div className={styles.galleryContainer}>
       <div className={styles.heroBorder}>
-        <img
+        <Image
           className={styles.galleryImg}
           src={props.img}
           alt="An image of a divine being"
+          height={144}
+          width={144}
         />
       </div>
       <p className={styles.imgName}>{props.name}</p>
