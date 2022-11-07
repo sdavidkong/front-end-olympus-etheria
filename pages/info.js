499ee5ccb8956c61363ef9f5207360019d61a8a8
S David Kong <SDAVIDKONG@gmail.com>
2022-11-07 15:23:21 -0500

    created info page

--- pages/info.js
+++ pages/info.js
@@ -1,5 +1,54 @@
 import styles from "../styles/Home.module.css";
+import Link from "next/link";
 
 export default function Home() {
-  return <div className={styles.main}>INFO COMING SOON</div>;
+  return (
+    // <div className={styles.main}>
+    <div className={styles.infoSection}>
+      <div className={styles.infoText}>
+        {" "}
+        <h3>Project Info</h3>
+        <p>
+          -There are a total of 75 NFTs in the Olympus Etheria collection. There
+          will never be any more than 75. <br></br>
+          <br></br>
+          -There is no official discord channel for this project, beware of
+          anyone claiming that there is.<br></br>
+          <br></br>
+          -Any and all official communications relating to Olympus Etheria will
+          occur via our{" "}
+          <Link href="https://twitter.com/OlympusEtheria">
+            <a className={styles.twitterLink}>Twitter</a>
+          </Link>
+          .<br></br>
+          <br></br>
+          -Public minting will begin at an unspecified time on Nov. 16, 2022.
+          There is no pre-sale or allow list. <br></br>
+          <br></br>
+          -Mint price will be announced two days prior, on Nov. 14th. There will
+          be a limit of 1 mint per unique wallet.<br></br>
+        </p>
+      </div>
+      <div>
+        <div className={styles.heroBorder}>
+          <img
+            className={styles.hero}
+            src="/goddesses/22RafiyaTheStoryteller.png"
+            alt="An image of a badass african god surrounded by ether"
+          />
+        </div>
+      </div>
+
+      <div className={styles.infoText2}>
+        {" "}
+        <h3>Smart Contract</h3>
+        <p>
+          The Olympus Etheria smart contract is a verified 721A created using
+          mintplex.xyz on Ethereum Mainnet. The contract address will be posted
+          when the public mint is opened and announced.
+        </p>
+      </div>
+    </div>
+    // </div>
+  );
 }
